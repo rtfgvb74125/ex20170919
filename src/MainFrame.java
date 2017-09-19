2cc3ace577d4b1e22eb4a9ce83e68d89eddd2163
105021056 <rtfgvb74125@gmail.com>
2017-09-19 11:56:35 +0800

    test ex2

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,26 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
+    private Button bt = new Button("exit");
     public MainFrame(){
         init();
     }
     private void init(){
         this.setLocation(500,250);
         this.setSize(400,300);
+        this.setLayout(null);
+        this.add(bt);
+        bt.setBounds(200,150,50,50);
+        bt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
